d56fff71bb3ce29b661be5b9f588688ee9dfa47e
Teo-KJ <48685014+Teo-KJ@users.noreply.github.com>
2025-06-01 12:15:12 +0800

    Housekeeping: Clean up categories

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -31,7 +31,7 @@ const (
 
 	TransportCategory        = "Transport"
 	FoodCategory             = "Food"
-	EntertainmentCategory    = "Fitness and Entertainment"
+	EntertainmentCategory    = "Entertainment"
 	TravelCategory           = "Travel"
 	HealthAndFitnessCategory = "Health and Fitness"
 	EducationCategory        = "Education"
